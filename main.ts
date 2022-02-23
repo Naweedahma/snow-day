bf487678af28c5a901ea66bf0718eb772dd18d32
Naweedahma <100299521+Naweedahma@users.noreply.github.com>
2022-02-23 10:41:22 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    music.playMelody("C5 B A G F E D C ", 120)
+})
+basic.showIcon(IconNames.Heart)
+music.playMelody("E F G A A A A A ", 120)
+basic.forever(function () {
+	
+})
